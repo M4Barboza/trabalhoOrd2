020f166f4c30896bbb1370c8c885e623936bf240
M4Barboza <matheus.barboza0@gmail.com>
2021-12-02 16:37:16 -0300

    Arquivo único

--- main.c
+++ main.c
@@ -3,36 +3,37 @@
 #include <string.h>
 #include "defines.h"
 
-#include "criarArvoreB/gerenciador.c"
-#include "impressaoDasChavesOrdemCrescente.c"
-#include "impressaoArvore.c"
+#include "criarArvoreB/btree.c"
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv[]) {
-
-    if (argc == 3 && strcmp(argv[1], "-c") == 0) {
+    if (argc == 3 && strcmp(argv[1], "-c") == 0)
+    {
 
         printf("criação da arvore B ativada no arquivo = %s\n", argv[2]);
         gerenciador(argv[2]);
-
-    } else if (argc == 3 && strcmp(argv[1], "-p") == 0) {
+    }
+    else if (argc == 3 && strcmp(argv[1], "-p") == 0)
+    {
 
         printf("Impressão da árvore-B do arquivo = %s\n", argv[2]);
         impressaoArvoreB(argv[2]);
-
-    } else if (argc == 2 && strcmp(argv[1], "-k") == 0) {
+    }
+    else if (argc == 2 && strcmp(argv[1], "-k") == 0)
+    {
 
         printf("impressão das chaves em ordem crescente do arquivo %s\n", argv[2]);
         impressaoChavesOrdemCrescente(argv[2]);
-
-    } else {
+    }
+    else
+    {
 
         fprintf(stderr, "Argumentos incorretos!\n");
         fprintf(stderr, "Modo de uso:\n");
         fprintf(stderr, "$ %s (-c|-k) nome_arquivo\n", argv[0]);
         fprintf(stderr, "$ %s -p\n", argv[0]);
         exit(EXIT_FAILURE);
-
     }
 
     return 0;
--- struct.h
+++
@@ -1,8 +0,0 @@
-#include "defines.h"
-
-typedef struct {
-    
-    int quantidadeDeChaves;
-    char chave[MAXCHAVE];
-    int filhos[MAXCHAVE + 1];
-} PAG;
